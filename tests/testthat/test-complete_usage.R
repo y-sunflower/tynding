829bc6d2f496f7174dd2f93349cb1c2480a1cd52
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2026-04-09 10:18:45 +0200

    skip specific tests on CRAN

--- tests/testthat/test-complete_usage.R
+++ tests/testthat/test-complete_usage.R
@@ -24,6 +24,7 @@ test_that("Example 1: Pass additional named arguments to Typst, with JSON encodi
 })
 
 test_that("Example 2: A simple Typst document", {
+  skip_on_cran()
   input_file <- test_path("typst", "example-2.typ")
 
   pdf_file <- typst_compile(input_file)
