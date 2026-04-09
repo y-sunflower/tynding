207ed9601f2ce422c496e3b0a226c7858670797a
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2026-04-09 10:02:50 +0200

    skip specific tests on CRAN

--- tests/testthat/test-complete_usage.R
+++ tests/testthat/test-complete_usage.R
@@ -1,7 +1,14 @@
+# We skip tests on R CMD CHECK here because path resolution seems a bit too complex
+# in order to test the "root" argument of typst_compile().
+
 test_that("Example 1: Pass additional named arguments to Typst, with JSON encoding", {
+  skip_on_cran()
+  input_file <- test_path("typst", "example-1.typ")
+  output_file <- tempfile(fileext = ".pdf")
+
   pdf_file <- typst_compile(
-    "typst/example-1.typ",
-    output = "typst/example-11.pdf",
+    input_file,
+    output = output_file,
     title = "Quarterly report",
     author = "Joseph",
     persons = list(
@@ -11,29 +18,51 @@ test_that("Example 1: Pass additional named arguments to Typst, with JSON encodi
     )
   )
 
-  expect_true(pdf_file == "typst/example-11.pdf")
+  expect_true(pdf_file == output_file)
   expect_true(file.exists(pdf_file))
+  unlink(output_file)
 })
 
 test_that("Example 2: A simple Typst document", {
-  pdf_file <- typst_compile("typst/example-2.typ")
+  skip_on_cran()
+  input_file <- test_path("typst", "example-2.typ")
+
+  pdf_file <- typst_compile(input_file, output = output_file)
   expect_true(file.exists(pdf_file))
+  unlink(pdf_file)
 })
 
 test_that("Example 3: Use font from a parent directory", {
+  skip_on_cran()
+  input_file <- test_path("typst", "subdir", "example-3.typ")
+  root_dir <- test_path("typst")
+  font_path <- test_path("fonts")
+  output_file <- tempfile(fileext = ".pdf")
+
   pdf_file <- typst_compile(
-    "typst/subdir/example-3.typ",
-    root = "typst",
-    font_path = "fonts"
+    input_file,
+    output = output_file,
+    root = root_dir,
+    font_path = font_path
   )
+
   expect_true(file.exists(pdf_file))
+  expect_true(pdf_file == output_file)
+  unlink(output_file)
 })
 
 test_that("Example 4: More complex usage", {
-  pdf_file <- typst_compile(
-    "typst/example-4.typ",
+  skip_on_cran()
+  input_file <- test_path("typst", "example-4.typ")
+  output_file <- tempfile(fileext = ".png")
+
+  png_file <- typst_compile(
+    input_file,
+    output = output_file,
     output_format = "png",
     food = "cookies"
   )
-  expect_true(file.exists(pdf_file))
+  expect_true(file.exists(png_file))
+  expect_true(png_file == output_file)
+  unlink(output_file)
 })
--- tests/testthat/test-typst.R
+++ tests/testthat/test-typst.R
@@ -1,55 +1,38 @@
-test_font_path <- test_path("fonts")
-
 test_that("Valid Typst compile usage", {
-  if (!dir.exists(test_font_path)) {
-    dir.create(test_font_path)
-  }
+  generated_files <- character(0)
+  on.exit(unlink(generated_files), add = TRUE)
 
   markup <- c("= Hello World", "This is a Typst document.")
   typ_file <- typst_write(markup)
+  generated_files <- c(generated_files, typ_file)
   expect_true(file.exists(typ_file))
 
   pdf_file <- typst_compile(typ_file)
+  generated_files <- c(generated_files, pdf_file)
   expect_true(file.exists(pdf_file))
 
   pdf_file <- tempfile(fileext = ".pdf")
+  generated_files <- c(generated_files, pdf_file)
   typst_compile(typ_file, output = pdf_file)
   expect_true(file.exists(pdf_file))
 
   html_file <- typst_compile(typ_file, output_format = "html")
+  generated_files <- c(generated_files, html_file)
   expect_true(file.exists(html_file))
 
   png_file <- typst_compile(typ_file, output_format = "png")
+  generated_files <- c(generated_files, png_file)
   expect_true(file.exists(png_file))
 
   svg_file <- typst_compile(typ_file, output_format = "svg")
+  generated_files <- c(generated_files, svg_file)
   expect_true(file.exists(svg_file))
-
-  markup <- c(
-    '#set text(font: "Ultra")',
-    "= Hello World",
-    "This is a Typst document."
-  )
-  typ_file <- typst_write(markup)
-  pdf_file <- typst_compile(typ_file, font_path = test_font_path)
-  expect_true(file.exists(pdf_file))
-
-  markup <- c(
-    '#set text(font: "Ultra")',
-    '#set document(title: "Title of the document")',
-    "= Hello World",
-    "This is a Typst document."
-  )
-  typ_file <- typst_write(markup)
-  pdf_file <- typst_compile(
-    typ_file,
-    font_path = test_font_path,
-    pdf_standard = "ua-1"
-  )
-  expect_true(file.exists(pdf_file))
 })
 
 test_that("Invalid Typst CLI usage", {
+  generated_files <- character(0)
+  on.exit(unlink(generated_files), add = TRUE)
+
   markup <- c("= Hello World", "This is a Typst document.")
   expect_error(
     typst_write(markup, output = "invalid.py"),
@@ -62,6 +45,7 @@ test_that("Invalid Typst CLI usage", {
     "This is a Typst document."
   )
   typ_file <- typst_write(markup)
+  generated_files <- c(generated_files, typ_file)
   expect_error(
     typst_compile(
       typ_file,
