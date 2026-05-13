2b59b37a247a34c7ae8abe991b08a1256cd968ed
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2026-05-13 15:44:32 +0200

    add tests for error messages

---
+++ tests/testthat/test-error_messages.R
@@ -0,0 +1,56 @@
+test_that("Unknown variable produces a rich error message", {
+  input_file <- test_path("typst", "error-unknown-variable.typ")
+
+  expect_error(
+    typst_compile(input_file),
+    "error: unknown variable: undefined_variable
+  ┌─ error-unknown-variable.typ:2:2
+  │
+2 │ #undefined_variable
+  │  ^^^^^^^^^^^^^^^^^^",
+    fixed = TRUE
+  )
+})
+
+test_that("Type mismatch on set rule produces a rich error message", {
+  input_file <- test_path("typst", "error-type-mismatch.typ")
+
+  expect_error(
+    typst_compile(input_file),
+    'error: expected length, found string
+  ┌─ error-type-mismatch.typ:1:17
+  │
+1 │ #set text(size: "big")
+  │                 ^^^^^',
+    fixed = TRUE
+  )
+})
+
+test_that("Missing import file produces a rich error message", {
+  input_file <- test_path("typst", "error-missing-import.typ")
+
+  expect_error(
+    typst_compile(input_file),
+    'error: file not found (searched at /Users/josephbarbier/l/tynding/tests/testthat/typst/does-not-exist.typ)
+  ┌─ error-missing-import.typ:1:9
+  │
+1 │ #import "does-not-exist.typ": *
+  │         ^^^^^^^^^^^^^^^^^^^^',
+    fixed = TRUE
+  )
+})
+
+test_that("More complex error message", {
+  input_file <- test_path("typst", "error-complete.typ")
+
+  expect_error(
+    typst_compile(input_file),
+    "error: unknown variable: this-variable-doesnt-exist
+  ┌─ error-complete.typ:6:2
+  │
+6 │ #this-variable-doesnt-exist
+  │  ^^^^^^^^^^^^^^^^^^^^^^^^^^
+hint: if you meant to use subtraction, try adding spaces around the minus signs: `this - variable - doesnt - exist`",
+    fixed = TRUE
+  )
+})
---
+++ tests/testthat/typst/error-complete.typ
@@ -0,0 +1,9 @@
+#set page(fill: green)
+#circle(fill: red)
+
+#pagebreak()
+
+#this-variable-doesnt-exist
+
+#set page(fill: yellow)
+#circle(fill: blue)
